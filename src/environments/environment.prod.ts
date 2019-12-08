b487bfcf1cc42dc64bd324d2827a5b769f3d4ecc
thatcarguy <5158433+thatcarguy@users.noreply.github.com>
2019-12-07 22:55:13 -0700

    Fixed code styling issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "http://youtubemp3-env-2.pjs34aejum.us-east-2.elasticbeanstalk.com/api"
+  apiUrl: 'http://youtubemp3-env-2.pjs34aejum.us-east-2.elasticbeanstalk.com/api'
 };
